879f7bcbd2aa782e3997943cb2da04a498aae6da
Syncxv <aruldevarackal1@gmail.com>
2022-09-02 14:59:14 +0530

    yay

--- src/injector/rust/src/main.rs
+++ src/injector/rust/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let steam_path = &args[1];
+
+        
+    let output = Command::new(steam_path)
+        .arg("-dev")
+        .spawn()
+        .expect("welp wrong steam path probably");
+    let steam_instance = output.stdout;
+    println!("{:?}", steam_instance);
+
 }
