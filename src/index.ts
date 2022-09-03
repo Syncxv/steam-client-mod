fcaabbdb77e39dadd0096d6348128acf809193fd
Syncxv <aruldevarackal1@gmail.com>
2022-09-03 15:34:40 +0530

    name change

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { Webpack } from './code_modules/webpack';
 
 console.log('WHAT THE FUCK bro');
 
-class Bruh {
+class Steamed {
     webpack = new Webpack();
     constructor() {
         this.webpack.initalize();
     }
 }
 
-(window as any).bruh = new Bruh();
+(window as any).steamed = new Steamed();
