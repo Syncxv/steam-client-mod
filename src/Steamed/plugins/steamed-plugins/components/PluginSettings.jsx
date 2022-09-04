959cc28a4a458478704ff37c07bf47294c9cc6f2
Syncxv <aruldevarackal1@gmail.com>
2022-09-05 02:39:06 +0530

    SHEEESH PLUGINS PAGE

--- src/Steamed/plugins/steamed-plugins/components/PluginSettings.jsx
+++ src/Steamed/plugins/steamed-plugins/components/PluginSettings.jsx
@@ -1,1 +1,22 @@
-module.exports = () => {};
+const { SwitchItem } = require('../../../../code_modules/components');
+
+module.exports = () => {
+    return (
+        <form className="DialogBody">
+            <div className="SettingsGroup">
+                {[...steamed.pluginManager.plugins.values()].map((plugin) => (
+                    <SwitchItem
+                        label={plugin.manifest.name}
+                        description={plugin.manifest.description}
+                        onChange={(val) => (val ? steamed.pluginManager.enable(plugin.entityID) : steamed.pluginManager.disable(plugin.entityID))}
+                        checked={plugin.ready}
+                    />
+                ))}
+            </div>
+        </form>
+    );
+};
+
+const Plugin = (manifest) => {
+    return <div className="plugin"></div>;
+};
--- src/Steamed/plugins/steamed-plugins/index.js
+++ src/Steamed/plugins/steamed-plugins/index.js
@@ -1,12 +1,14 @@
 const Plugin = require('../../../code_modules/entities/Plugin');
 
+const PluginSettings = require('./components/PluginSettings');
+
 module.exports = class PluginsSection extends Plugin {
     manifest = { name: 'Plugins Settings', description: 'the plugins section in settings HEHEH HA', author: 'Aria' };
     startPlugin() {
         steamed.api.settings.registerSetting(this.entityID, {
             title: 'Plugins',
             identifier: 'steamed-plugins',
-            content: steamed.webpack.common.React.createElement('div', {}, 'plugins gang'),
+            content: steamed.webpack.common.React.createElement(PluginSettings),
         });
     }
 };
