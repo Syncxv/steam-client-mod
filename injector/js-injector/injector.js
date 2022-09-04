308bcd71caabbd7200836dd81318c3037f4977ee
Syncxv <aruldevarackal1@gmail.com>
2022-09-04 15:50:13 +0530

    YOOOOOOOOOOOOOOOO
    ok now time to inject the script tag eh

--- injector/js-injector/injector.js
+++ injector/js-injector/injector.js
@@ -1,6 +1,18 @@
+let sleep = (ms) => new Promise((res) => setTimeout(() => res, ms));
 const main = async () => {
     const steamedDist = await (await fetch('./steamed.js')).text();
-    document.getElementById('tracked_frame_friends_chat').contentWindow.eval(steamedDist);
+
+    let goodWindow = document.getElementById('tracked_frame_friends_chat').contentWindow;
+
+    function wait() {
+        if (!condition) {
+            setTimeout(wait, 100);
+        } else {
+            // CODE GOES IN HERE
+            goodWindow.eval(steamedDist);
+        }
+    }
+    wait();
 };
 
 main().catch((err) => console.error(err));
