8fc5d48211e2914bd6405ebe42a3185e4a6e4c34
Syncxv <aruldevarackal1@gmail.com>
2022-10-19 20:37:52 +0530

    i18n

--- src/modules/webpack/common.ts
+++ src/modules/webpack/common.ts
@@ -2,7 +2,10 @@ import { findByProps } from './webpack';
 
 export let React: typeof import('React');
 export let MessageClass: any;
+export let i18n: any;
+
 export const initCommon = () => {
     React = findByProps('useState');
+    i18n = steamed.Webpack.findByProps('LocalizeString');
     MessageClass = steamed.Webpack.find((m) => m?.prototype?.constructor.toString().includes('eErrorSendingObservable'));
 };
