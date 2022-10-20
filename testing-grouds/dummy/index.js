eb3cde9e674b8373227f4f20fb86356aad8f8cb9
Syncxv <aruldevarackal1@gmail.com>
2022-10-20 06:39:11 +0530

    hi

--- testing-grouds/dummy/index.js
+++
@@ -1,8 +0,0 @@
-const { Plugin } = require('steamed/entities');
-
-export class Dummy extends Plugin {
-    manifest = { name: 'Dummy Plugin', description: 'this plugin does nothing', author: 'Aria' };
-    startPlugin() {
-        console.log('i do nothing');
-    }
-}
--- testing-grouds/steamed-commands/commands/index.js
+++
@@ -1,9 +0,0 @@
-require
-    .context('./', true, /\js$/)
-    .keys()
-    .filter((file) => file !== './index.js')
-    .forEach((filename) => {
-        const moduleName = filename.split('./')[1];
-        console.log(moduleName);
-        exports[moduleName] = require(`${filename}`);
-    });
