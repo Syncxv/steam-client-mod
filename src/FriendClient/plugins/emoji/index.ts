71077e11db3040e71c17dc1c8b23478f874c4823
Syncxv <aruldevarackal1@gmail.com>
2022-10-20 06:01:58 +0530

    hmmm on emoji selected dont work

---
+++ src/FriendClient/plugins/emoji/index.ts
@@ -0,0 +1,43 @@
+import { definePlugin } from '../../../modules/util/definePlugin';
+import { emojiObj } from './emojiObjs';
+
+export default definePlugin({
+    name: 'EmojiStuff',
+    description: 'this plugin adds unicode emoji gg ez',
+    authors: [{ name: 'Aria', discordId: '0' }],
+    version: '1.1.1',
+    patches: [
+        {
+            match: /(static GetEmoticonURL\(.{1,4}\){)/,
+            replace: '$1console.log("hi", e, t);\n',
+        },
+        {
+            match: /(GetEmoticonURL\(.{1,4}\);return.{1,4}\.createElement.{1,150})(.{1,2}\(\)\.createElement\("img".{1,250}\),)/,
+            // match: /(GetEmoticonURL\(.{1,4}\);return.{1,4}\.createElement.{1,150})(.{1,2}\(\)\.createElement\("img",.{1,50}className:(\(.{1,50}\))).{1,250}\)\),/,
+            replace: '$1emojiObj[e] != null ?  steamed.Webpack.Common.React.createElement("div", {className:"cool"}, emojiObj[e].emoji) : $2',
+        },
+
+        {
+            match: /(OnEmoticonSelected\(.{1,6}\){)/,
+            replace: '$1console.log(e, this);',
+        },
+    ],
+
+    async start() {
+        window.emojiObj = emojiObj;
+        function wait(): any {
+            if (!g_FriendsUIApp.ChatStore.EmoticonStore.BInitialized() || !g_FriendsUIApp.ChatStore.EmoticonStore.m_rgEmoticons.length) {
+                return setTimeout(wait, 1);
+            }
+            console.log('READY?', g_FriendsUIApp.ChatStore.EmoticonStore.m_rgEmoticons);
+            g_FriendsUIApp.ChatStore.EmoticonStore.m_rgEmoticons = g_FriendsUIApp.ChatStore.EmoticonStore.m_rgEmoticons.concat(
+                Object.values(emojiObj).map((e) => ({ name: e.name, last_used: 1663606897, use_count: 1, is_steamed: true }))
+            );
+        }
+        wait();
+    },
+
+    stop() {
+        g_FriendsUIApp.ChatStore.EmoticonStore.m_rgEmoticons = g_FriendsUIApp.ChatStore.EmoticonStore.m_rgEmoticons.filter((e: any) => !e.is_steamed);
+    },
+});
