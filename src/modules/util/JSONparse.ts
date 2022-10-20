c01bedf87ac518e0d497da22fd65cc9a6e32b3fe
Syncxv <aruldevarackal1@gmail.com>
2022-10-20 13:27:40 +0530

    PHEW THAT SCARED ME
    man im stupid

---
+++ src/modules/util/JSONparse.ts
@@ -0,0 +1,8 @@
+export const JSONparse = (str: string, defaultVal: any = {}) => {
+    try {
+        return JSON.parse(str);
+    } catch (err) {
+        console.error('failed to parse json', err);
+        return defaultVal;
+    }
+};
