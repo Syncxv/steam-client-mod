d04f134add4f72c9ad60bc14b3501f66fcc4608c
Syncxv <aruldevarackal1@gmail.com>
2022-10-22 10:33:28 +0530

    some new themes

---
+++ src/themes/newsteamchat/index.ts
@@ -0,0 +1,10 @@
+import { defineTheme } from '../../modules/util/defineTheme';
+import css from './theme.css';
+
+export default defineTheme({
+    name: 'New Steam Chat',
+    description: 'idk man HEHEH HA',
+    authors: [{ name: 'idk but not me' }],
+    type: 'friend',
+    css,
+});
