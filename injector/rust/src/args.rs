43e5198810c0c59f8ec60c04818cf6b24b3c4b1f
Syncxv <aruldevarackal1@gmail.com>
2023-01-07 12:48:36 +1100

    yay steam path

---
+++ injector/rust/src/args.rs
@@ -0,0 +1,14 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct SteamInjectorCli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// does testing things
+    Test,
+}
--- injector/rust/src/main.rs
+++ injector/rust/src/main.rs
@@ -1,7 +1,26 @@
+mod args;
 mod config;
 
+use std::collections::HashMap;
+
+use args::{Commands, SteamInjectorCli};
+use clap::Parser;
+
 fn main() {
-    println!("hi");
     let steam_config = config::get_config();
-    println!("{:?}", steam_config);
+
+    let args = SteamInjectorCli::parse();
+    match args.command {
+        None => {
+            println!("hi there")
+        }
+        Some(x) => match x {
+            Commands::Test => handle_test(steam_config),
+        },
+    }
+}
+
+fn handle_test(steam_config: HashMap<String, String>) {
+    let what = steam_config.get("steam_path").unwrap();
+    println!("TEST GANG and steam path is {}", what);
 }
