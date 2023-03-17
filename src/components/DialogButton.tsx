44ab6d2050aac193017369c62f638817b75e6e8a
Syncxv <aruldevarackal1@gmail.com>
2023-03-17 20:54:08 +1100

    add Container and DialogButton

---
+++ src/components/DialogButton.tsx
@@ -0,0 +1,36 @@
+type ValueOf<T> = T[keyof T]
+
+type DialogButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+	children: React.ReactNode
+}
+
+const DialogVriants = {
+	Variant: {
+		Primary: 'Primary',
+		Secondary: 'Secondary',
+		GreenPlay: 'GreenPlay'
+	} as const
+} as const
+
+type DialogButtonType = React.FC<
+	DialogButtonProps &
+		Partial<{
+			variant: ValueOf<typeof DialogVriants.Variant>
+		}>
+>
+
+export const DialogButton: DialogButtonType = ({
+	children,
+	className,
+	variant = DialogVriants.Variant.Secondary,
+	...props
+}) => {
+	return (
+		<button
+			className={`DialogButton _DialogLayout Focusable ${variant} ${className ?? ''}`}
+			{...props}
+		>
+			{children}
+		</button>
+	)
+}
