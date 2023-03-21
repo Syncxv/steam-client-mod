ce3eed04edccee1a199fb97ba6481260fcd9fa2d
Syncxv <aruldevarackal1@gmail.com>
2023-03-21 21:40:04 +1100

    PLUGINS SETTINGS PAGE

---
+++ src/plugins/steamed-plugins/index.tsx
@@ -0,0 +1,15 @@
+import { Devs } from '@utils/constants'
+import { definePlugin } from '@utils/definePlugin'
+import { Settings } from './components/Settings'
+
+export default definePlugin({
+	name: 'Plugins',
+	description: 'Idk Man Themes eh',
+	authors: [Devs.Aria],
+	version: '1.1.1',
+	settingsComponent: {
+		title: 'Plugins',
+		identifier: 'steamed-plugins',
+		content: () => <Settings />
+	}
+})
