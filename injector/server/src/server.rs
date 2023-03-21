f6c7abf026af83866f81b73a5d6f694d96ae2de4
Syncxv <aruldevarackal1@gmail.com>
2023-03-21 21:39:19 +1100

    dont need to unpatch cuse -noverfiyfiles

--- injector/server/src/server.rs
+++ injector/server/src/server.rs
@@ -9,10 +9,10 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 #[get("/shutdown")]
 async fn hello() -> impl Responder {
     println!("shutting down");
-    let curr_dir = env::current_dir().unwrap().to_str().unwrap().to_string();
-    let mut command = Command::new(Path::new(&curr_dir).join("injector.exe"));
-    command.arg("unpatch-friend");
-    command.spawn().unwrap();
+    //let curr_dir = env::current_dir().unwrap().to_str().unwrap().to_string();
+    // let mut command = Command::new(Path::new(&curr_dir).join("injector.exe"));
+    // command.arg("unpatch-friend");
+    // command.spawn().unwrap();
     exit(0);
     HttpResponse::Ok().body("hey")
 }
