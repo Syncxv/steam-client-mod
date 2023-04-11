ef8a6cd37b31c356d66a6ebbb1965e48e786d795
Syncxv <aruldevarackal1@gmail.com>
2023-04-11 15:58:32 +1000

    start of installer

---
+++ installer/vite.config.ts
@@ -0,0 +1,45 @@
+/*
+ * Steamed, a modification for the Steam Client
+ * Copyright (c) 2023 Syncxv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+	plugins: [solidPlugin()],
+
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	// prevent vite from obscuring rust errors
+	clearScreen: false,
+	// tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 1420,
+		strictPort: true,
+	},
+	// to make use of `TAURI_DEBUG` and other env variables
+	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+	envPrefix: ["VITE_", "TAURI_"],
+	build: {
+		// Tauri supports es2021
+		target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
+		// don't minify for debug builds
+		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+		// produce sourcemaps for debug builds
+		sourcemap: !!process.env.TAURI_DEBUG,
+	},
+}));
